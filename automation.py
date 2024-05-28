f3f09ee196414c04fe56beb49e37570b92a9d604
Fernando Antun <156030831+FernandoAlves710@users.noreply.github.com>
2024-05-28 15:20:22 -0300

    Update automation.py

--- automation.py
+++ automation.py
@@ -5,12 +5,13 @@ from io import BytesIO
 
 def extrair_texto(pdf_file):
     text = ""
+    filename = pdf_file.name
     with pdf_file:
         pdf_document = fitz.open(BytesIO(pdf_file.read()))
         for page_num in range(pdf_document.page_count):
             page = pdf_document.load_page(page_num)
             text += page.get_text()
-    return text
+    return text, filename
 
 def extrair_informacoes(texto, pagina):
     # Simulação de dados para demonstração
@@ -45,26 +46,25 @@ def main():
 
     pdf_file = st.file_uploader("Selecione o arquivo PDF", type="pdf")
     if pdf_file is not None:
+        texto, filename = extrair_texto(pdf_file)
+        st.write(f"PDF selecionado: {filename}")
+
         st.write("1. Pagina do BP:")
-        texto = extrair_texto(pdf_file)
         tabela_bp = extrair_informacoes(texto, 1)
         if tabela_bp is not None:
             st.table(tabela_bp)
 
         st.write("2. Pagina da DRE:")
-        texto = extrair_texto(pdf_file)
         tabela_dre = extrair_informacoes(texto, 2)
         if tabela_dre is not None:
             st.table(tabela_dre)
 
         st.write("3. Pagina do FC:")
-        texto = extrair_texto(pdf_file)
         tabela_fc = extrair_informacoes(texto, 3)
         if tabela_fc is not None:
             st.table(tabela_fc)
 
         st.write("4. Pagina da NECD:")
-        texto = extrair_texto(pdf_file)
         tabela_necd = extrair_informacoes(texto, 4)
         if tabela_necd is not None:
             st.table(tabela_necd)
